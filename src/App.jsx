4fc03258265d054eb352b4d13d4b2c00e0c2c7cf
Mayowa-de <mayortech03@gmail.com>
2026-06-17 17:00:27 -0700

    accept merge changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
 import Header from "./Component/Header"
 import SliderSection from "./Component/SliderSection"
 import Main from "./Component/Main"
 import "./index.css"
-=======
 
-
->>>>>>> 301ec69c0a2e794b4c90ad2a755693b8d99e89cb
 function App() {
   
-
   return (
-<<<<<<< HEAD
     <div className=" w-full flex flex-col items-center ">
       <Header />
       <SliderSection />
       <Main/>
-    </div>
-=======
-    <div></div>
->>>>>>> 301ec69c0a2e794b4c90ad2a755693b8d99e89cb
-
-  )
+      </div>  
+      )
 }
 
 export default App
