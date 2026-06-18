549ac23ea0eb07a823db5cc50b5512bd8f25cce9
Mayowa-de <mayortech03@gmail.com>
2026-06-18 11:22:32 -0700

    added HistorySection.jsx file and increase the width and height of the convert button

--- src/Component/FirstCardSection.jsx
+++ src/Component/FirstCardSection.jsx
@@ -3,15 +3,15 @@ import Exchange from '../assets/images/icon-exchange.svg'
 
 export default function FirstCardSection() {
   return (
-    <section className='flex flex-col w-[1036px] h-[263px] gap-[16px]'>
+    <section className='flex flex-col w-[1036px] h-[931px] gap-[16px]'>
       <h1 className='laptop:text-[20px] text-[jetbrains-mono, regular] tracking-[-0.5px] leading-[120%] text-[#FFFFFF]'>CHECK RATE</h1>
 
       <div className='flex flex-col  bg-[#202022] px-[20px] p-[20px] rounded-[24px] gap-[24px]'>
-        <div className='flex gap-[8px] items-center'>
-          <div className='w-full px-[20px] p-[20px] gap-[20px] rounded-[16px] bg-[#2E2E2E] border-[#3D3D3D] border-[1px]'>
+        <div className='flex gap-[8px] items-center justify-center'>
+          <div className='laptop:w-[450px] w-full laptop:h-[118px] px-[20px] p-[20px] gap-[20px] rounded-[16px] bg-[#2E2E2E] border-[#3D3D3D] border-[1px]'>
             <h2 className='text-[#C6C6C6] text-[14px] tracking-[1px]'>SEND</h2>
             <div className='flex justify-between gap-[auto]'>
-              <input type="text" className='w-[123px] h-[40px] text-[#FFFF] bg-neutral-600 px-[3px] text-[2rem] focus:ring-2 focus-within:ring-[#CEF739] rounded-[8px] border-none focus:border-[2px] outline-none focus:border-[#CEF739]' />
+              <input type="text" className='w-[123px] h-[40px] bg-transparent text-[#FFFF] focus:border-b-[2px] px-[3px] text-[2rem] focus:ring-2 focus-within:ring-[#CEF739] rounded-[8px] border-none focus:border-[2px] outline-none focus:border-[#CEF739]' />
               <div className='w-[96px] h-[40px] gap[8px] bg-[#2E2E2E] border-[1px] border-[#3D3D3D] p-[10px] px-[10px] rounded-[8px] '>
                 <select name="" id="" className='bg-transparent appearance-none focus:outline-none text-white border-none text-[14px] tracking-[1px]'>
                   <option value="USD" className='cursor-pointer'>USD</option>
@@ -21,13 +21,13 @@ export default function FirstCardSection() {
               </div>
             </div>
           </div>
-          <div className='px-[10px] p-[10px] bg-[#2E2E2E] rounded-[8px]'>
-            <img src={Exchange} alt="exchange-icon" />
+          <div className='w-[48px] h-[48px] px-[10px] p-[10px] bg-[#2E2E2E] rounded-[8px] items-center flex justify-center focus:border-2 focus:border-[#CEF739]' role='button'>
+            <img src={Exchange} alt="exchange-icon" className='w-[20px] h-[20px] items-center flex' />
           </div>
-          <div className='w-full px-[20px] p-[20px] gap-[20px] rounded-[16px] bg-[#2E2E2E] border-[#3D3D3D] border-[1px]'>
+          <div className='laptop:w-[450px] w-full laptop:h-[118px] px-[20px] p-[20px] gap-[20px] rounded-[16px] bg-[#2E2E2E] border-[#3D3D3D] border-[1px]'>
             <h2 className='text-[#C6C6C6] text-[14px] tracking-[1px]'>RECEIVE</h2>
             <div className='flex justify-between gap-[auto]'>
-              <input type="text" className='w-[123px]  h-[40px] bg-neutral-600 text-[#FFFF] pr-[1px] px-[5px] text-[2rem]  rounded-[8px] border-none focus:ring-[2px] focus:outline-none focus:ring-[#CEF739]' />
+              <input type="text" className='w-[123px]  h-[40px] bg-transparent focus:border-b-[2px] text-[#CEF739] pr-[1px] px-[5px] text-[2rem]  rounded-[8px] border-none focus:ring-[2px] focus:outline-none focus:ring-[#CEF739]' />
               <div className='w-[96px] h-[40px] gap[8px] bg-[#2E2E2E] border-[1px] border-[#3D3D3D] p-[10px] px-[10px] rounded-[8px] '>
                 <select name="" id="" className='bg-transparent appearance-none focus:outline-none text-white border-none text-[14px] tracking-[1px]'>
                   <option value="USD" className='cursor-pointer'>USD</option>
@@ -37,8 +37,10 @@ export default function FirstCardSection() {
               </div>
             </div>
           </div>
-
         </div>
+
+        <hr className='text-[5px] border-dashed border-[1px] border-neutral-900 leading-1' />
+
         <div className='flex justify-between items-center'>
           <div></div>
 
