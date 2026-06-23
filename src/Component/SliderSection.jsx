677afe04288b7decbf57d891af0aa62c56737462
Mayowa-de <mayortech3@gmail.com>
2026-06-23 10:20:07 +0100

    Adjust spacing and border styles in SliderSection

--- src/Component/SliderSection.jsx
+++ src/Component/SliderSection.jsx
@@ -37,7 +37,7 @@ export default function SliderSection() {
         animate={{ x: ["0%", "-50%"] }}
         transition={{ duration: 60, repeat: Infinity, ease: "linear" }}>
         {[...(currentData || []), ...(currentData || [])].map((currency, index) => (
-          <div key={`${currency.code}-${index}`} className='flex flex-row gap-[12px] border-r-[2px] border-r-white/20'>
+          <div key={`${currency.code}-${index}`} className='flex flex-row gap-[8px] border-r border-r-white/20 px-[4px]'>
             <li className='text-[#FFFFFF] text-[10px] md:text-[12px] tracking-[1px] font-semibold list-none'>
               {currency.base}/{currency.code}
             </li>
