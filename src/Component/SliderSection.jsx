30832ca94c87d7c18c3cda7193530274c117bdcf
Mayowa-de <mayortech3@gmail.com>
2026-06-24 21:50:50 +0100

    Refactor SliderSection component for better structure

--- src/Component/SliderSection.jsx
+++ src/Component/SliderSection.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { motion } from "framer-motion"
-import chevronIcon from "../assets/images/icon-chevron-down.svg"
 
 export default function SliderSection({ baseCurrency }) {
   const [currentData, setCurrentData] = useState([])
@@ -45,7 +44,7 @@ export default function SliderSection({ baseCurrency }) {
             </li>
             <li className={`flex items-center justify-center text-[10px] md:text-[12px] tracking-[1px] font-semibold list-none 
               ${currency.percentageChange > 0 ? 'text-green-500' : 'text-red-500'}`}>
-              <span className='w-[12px] h-[12px] flex items-center px-[4px]' > {currency.percentageChange > 0 ? "\u25B2" : "\u25BC"}</span>
+              <span className='w-[12px] h-[12px] flex items-center justify-center px-[4px]' > {currency.percentageChange > 0 ? "\u25B2" : "\u25BC"}</span>
               {currency.percentageChange}%
             </li>
           </div>
