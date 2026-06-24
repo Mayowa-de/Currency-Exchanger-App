6186f60c72aa7e343882080e9362bd18522186a8
Mayowa-de <mayortech3@gmail.com>
2026-06-24 20:49:50 +0100

    Fetch currency codes and pass to FirstCardSection

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import FirstCardSection from './FirstCardSection'
 import HistorySection from './HistorySection'
 
 export default function Main({ baseCurrency, setBaseCurrency }) {
+const [getCode, setgetCode] = useState([])
+  
+  useEffect(() =>{
+  async function fetchCodeData() {
+      try {
+        const apiUrl = 'https://currency-exchanger-app-backend.onrender.com/api?code' || 'http://localhost:3000/api'
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+
+        setgetCode(
+          data.currencies?.map(currency => ({
+            ...currency,
+            code: data.code
+          })) || []
+        )
+      } catch (error) {
+        console.log("Error can't fetch api", error)
+      }
+    }
+    fetchCodeData()
+  }, [])
   return (
     <div className='flex flex-col items-center justify-center  md:p-[48px] md:w-[1100px] md:h-[607px] w-full p-[30px] px-[10px]  md:px-[20px]'>
-        <FirstCardSection baseCurrency={baseCurrency} setBaseCurrency={setBaseCurrency} />
+        <FirstCardSection baseCurrency={baseCurrency} setBaseCurrency={setBaseCurrency} getCode={getCode} setgetCode={setgetCode} />
         <HistorySection/>
     </div>
   )
