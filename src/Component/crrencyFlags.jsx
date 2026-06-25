f93170a981a60e6bec5422f7e5a431d9f34e728c
Mayowa-de <mayortech3@gmail.com>
2026-06-25 10:02:22 +0100

    Update flag image path in crrencyFlags.jsx

--- src/Component/crrencyFlags.jsx
+++ src/Component/crrencyFlags.jsx
@@ -1,7 +1,7 @@
-const flags = import.meta.glob('../Component/assets/images/flags/*.webp', { eager: true });
+const flags = import.meta.glob('../assets/images/flags/*.webp', { eager: true });
 
 export const getFlag = (code) =>{
    const country = code.slice(0, 2).toLowerCase()
-   const key = `../Component/assets/images/flags/${country}.webp`
-   return flags[key]?.default;
+   const key = `../assets/images/flags/${country}.webp`
+   return flags[key]?.default ?? flags[key];
 }
