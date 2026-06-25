12c13e4df7d14a25f07d75c33d09e384dd16305d
Mayowa-de <mayortech3@gmail.com>
2026-06-25 09:36:58 +0100

    Fix data-align-trigger condition in select component

--- src/components/ui/select.jsx
+++ src/components/ui/select.jsx
@@ -62,7 +62,7 @@ function SelectContent({
     <SelectPrimitive.Portal>
       <SelectPrimitive.Content
         data-slot="select-content"
-        data-align-trigger={position}
+        data-align-trigger={position === "item-aligned"}
         className={cn(
           "relative z-50 max-h-[300px] min-w-36 origin-(--radix-select-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-lg bg-popover text-popover-foreground shadow-md ring-1 ring-foreground/10 duration-100 data-[align-trigger=true]:animate-none data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
           position ==="popper"&&"data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
@@ -74,7 +74,7 @@ function SelectContent({
         <SelectPrimitive.Viewport
           data-position={position}
           className={cn(
-            "max-h-[300px] data-[position=popper]:w-full data-[position=popper]:min-w-(--radix-select-trigger-width)",
+            "p-1 overflow-y-auto data-[position=popper]:w-full data-[position=popper]:min-w-(--radix-select-trigger-width)",
             position === "popper" && ""
           )}>
           {children}
@@ -110,7 +110,7 @@ function SelectItem({
       )}
       {...props}>
       <span
-        className="pointer-events-none absolute text-white/20 right-2 flex size-4 items-center justify-center">
+        className="pointer-events-none absolute text-white right-2 flex size-4 items-center justify-center">
         <SelectPrimitive.ItemIndicator>
           <CheckIcon className="pointer-events-none" />
         </SelectPrimitive.ItemIndicator>
