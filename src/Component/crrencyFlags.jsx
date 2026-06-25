34f2110251696cbbf03eea8386671f7e27c002f6
Mayowa-de <mayortech3@gmail.com>
2026-06-25 10:07:41 +0100

    Refactor flag import and return logic
    Updated flag import to use 'as url' syntax and simplified return statement.

--- src/Component/crrencyFlags.jsx
+++ src/Component/crrencyFlags.jsx
@@ -1,7 +1,7 @@
-const flags = import.meta.glob('../assets/images/flags/*.webp', { eager: true });
+const flags = import.meta.glob('../assets/images/flags/*.webp', { eager: true }, as 'url');
 
 export const getFlag = (code) =>{
    const country = code.slice(0, 2).toLowerCase()
    const key = `../assets/images/flags/${country}.webp`
-   return flags[key]?.default ?? flags[key];
+   return flags[key];
 }
