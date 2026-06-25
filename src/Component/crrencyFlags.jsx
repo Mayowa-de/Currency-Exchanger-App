445ac9c1a5aedef82cc1d182710eba8d37faa272
Mayowa-de <mayortech3@gmail.com>
2026-06-25 10:11:14 +0100

    Fix syntax for import.meta.glob in crrencyFlags.jsx

--- src/Component/crrencyFlags.jsx
+++ src/Component/crrencyFlags.jsx
@@ -1,4 +1,4 @@
-const flags = import.meta.glob('../assets/images/flags/*.webp', { eager: true }, as 'url');
+const flags = import.meta.glob('../assets/images/flags/*.webp', { eager: true }, as: 'url');
 
 export const getFlag = (code) =>{
    const country = code.slice(0, 2).toLowerCase()
