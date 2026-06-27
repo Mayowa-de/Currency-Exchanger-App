c28972d599c31e7dc67af25faf30d4dbf50a3253
Mayowa-de <mayortech3@gmail.com>
2026-06-27 15:00:44 +0100

    Refactor Main component to use send/receive currencies

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import FirstCardSection from './FirstCardSection'
 import HistorySection from './HistorySection'
 
-export default function Main({ baseCurrency, setBaseCurrency }) {
+export default function Main({ baseSendCurrency, baseReceiveCurrency, setBaseSendCurrency, setBaseReceiveCurrency }) {
 const [getCode, setgetCode] = useState([])
   
   useEffect(() =>{
@@ -21,7 +21,7 @@ const [getCode, setgetCode] = useState([])
   }, [])
   return (
     <div className='flex flex-col items-center justify-center  md:p-[48px] md:w-[1100px] md:h-[607px] w-full p-[30px] px-[10px]  md:px-[20px]'>
-        <FirstCardSection baseCurrency={baseCurrency} setBaseCurrency={setBaseCurrency} options={getCode}  />
+        <FirstCardSection baseSendCurrency={baseSendCurrency} baseReceiveCurrency={baseReceiveCurrency} setBaseReceiveCurrency={setBaseReceiveCurrency} options={getCode}  />
         <HistorySection/>
     </div>
   )
