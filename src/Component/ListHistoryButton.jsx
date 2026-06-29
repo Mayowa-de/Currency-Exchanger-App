2b3840ef9a4a1af855a02a02a3b617021dac08f3
Mayowa-de <mayortech03@gmail.com>
2026-06-29 16:50:00 -0700

    change img element to picture

--- src/Component/ListHistoryButton.jsx
+++ src/Component/ListHistoryButton.jsx
@@ -6,8 +6,8 @@ export default function ListHistoryButton() {
     const ListTitle = ['HISTORY', 'COMPARE', 'FAVORITES', 'LOG']
     
   return (
-    <div className='laptop:w-[1036px] laptop:gap-[20px]'>
-      <ul className='flex  gap-[8px] text-[#FFFF]'>
+    <div className='laptop:w-[1036px] laptop:gap-[20px] mt-2'>
+      <ul className='md:flex hidden gap-[8px] text-[#FFFF]'>
         {ListTitle.map((tab)=>(
         <div className='flex flex-col gap-[4px]' key={tab} onClick={()=>{setisActive(tab)}} role='button'>
         <li className='p-[8px] text-[16px]'>{tab}</li>
@@ -15,7 +15,15 @@ export default function ListHistoryButton() {
         </div>
         ))}
       </ul>
-      <hr className='border-neutral-600'/>
+      <hr className='border-neutral-600 md:flex hidden'/>
+
+       <select className='flex w-full h-[40px] md:hidden rounded px-[12px] text-white bg-neutral-700 border-1 border-neutral-400' >
+        {ListTitle.map((tab) => (
+          <div className='flex' onClick={()=>{setisActive(tab)}}>
+          <option key={tab} >{tab}</option>
+          </div>
+        ))}
+        </select>
     </div>
   )
 }
