638725f0f0f57d617879b61774a3ac21a43133e8
Mayowa-de <mayortech3@gmail.com>
2026-07-02 09:36:15 +0100

    Remove HistorySectionCard from HistorySection
    Removed HistorySectionCard component from HistorySection.

--- src/Component/HistorySection.jsx
+++ src/Component/HistorySection.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ListHistoryButton from './ListHistoryButton'
-import HistorySectionCard from './HistorySectionCard'
 
 export default function HistorySection() {
   return (
     <div className='flex flex-col md:w-[1036px] px-[2px] w-full md:h-[540px] gap-[20px] '>
       <ListHistoryButton/>
-      <HistorySectionCard/>
     </div>
   )
 }
