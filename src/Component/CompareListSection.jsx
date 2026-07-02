2e20cdca9bb8640c6f330008d3ec5e08d08db719
Mayowa-de <mayortech3@gmail.com>
2026-07-02 10:14:13 +0100

    Implement currency fetching and display in CompareListSection
    Fetch currency data based on baseCurrency prop and display it in a list.

--- src/Component/CompareListSection.jsx
+++ src/Component/CompareListSection.jsx
@@ -1,1 +1,34 @@
+import {useState, useEffect} from 'react'
 
+
+const CompareListSection = ({baseCurrency}) =>{
+  const [currentData, setCurrentData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const apiUrl = `https://currency-exchanger-app-backend.onrender.com/api?base=${encodeURIComponent(baseCurrency)}` || 'http://localhost:3000/api'
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+
+        setCurrentData(
+          data.currencies?.map(currency => ({
+            ...currency,
+            base: data.base
+          })) || []
+        )
+      } catch (error) {
+        console.log("Error can't fetch api", error)
+      }
+    }
+    fetchData()
+  }, [baseCurrency])
+  return (
+    <div className='flex gap-[4px]>
+      {currentData.map((currency)=>(
+      <ul key={currency.code} >
+        <li>{currency}</li>
+      </ul>
+      </div>
+    )}
+export default CompareListSection
