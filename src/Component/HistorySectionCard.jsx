1962d657e2e9fbc2da01988076d5dd3975c8f91f
Mayowa-de <mayortech3@gmail.com>
2026-07-02 15:48:44 +0100

    Fetch currency data based on baseCurrency prop

--- src/Component/HistorySectionCard.jsx
+++ src/Component/HistorySectionCard.jsx
@@ -1,15 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
-export default function HistorySectionCard() {
+export default function HistorySectionCard({baseCurrency}) {
   const [isActiveDate, setisActiveDate] = useState("1M");
 
   const listDate =['1D','1W', '1M', '1Y', '5Y'];
+  const [currentData, setCurrentData] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const apiUrl = `https://currency-exchanger-app-backend.onrender.com/api?base=${encodeURIComponent(baseCurrency)}` || 'http://localhost:3000/api'
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+
+        setCurrentData(
+          data.currencies?.map(currency => ({
+            ...currency,
+            base: data.base
+          })) || []
+        )
+      } catch (error) {
+        console.log("Error can't fetch api", error)
+      }
+    }
+    fetchData()
+  }, [baseCurrency])
   return (
     <div className='md:flex-row flex flex-col md:justify-between gap-[16px]  md:items-center'>
       <div className='md:flex gap-[16px] grid grid-cols-2  w-full'>
         <div className='flex flex-col gap-[16px] text-neutral-400 md:w-[140px] w-full h-[81px] px-[20px] p-[12px] rounded-[16px] bg-[#202022]'>
             <p>OPEN</p>
-            <span className='text-neutral-50'>0.8516</span>
+            <span className='text-neutral-50'>{currency.rate}</span>
         </div>
         <div className='flex flex-col gap-[16px] text-neutral-400 md:w-[140px] w-full h-[81px] px-[20px] p-[12px] rounded-[16px] bg-[#202022]'>
             <p>LAST</p>
@@ -21,7 +42,7 @@ export default function HistorySectionCard() {
         </div>
         <div className='flex flex-col gap-[16px] text-neutral-400 md:w-[140px] w-full h-[81px] px-[20px] p-[12px] rounded-[16px] bg-[#202022]'>
             <p>% CHANGE</p>
-            <span className='text-green-500'>+0.16%</span>
+            <span className='text-green-500'>{currency.percentage}%</span>
         </div>
       </div>
 
