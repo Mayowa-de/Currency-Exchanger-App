9bcdc7fbed5d5b812e71a9bd7339570eb45e82bc
Mayowa-de <mayortech3@gmail.com>
2026-07-03 11:44:46 +0100

    Update list item layout in CompareListSection

--- src/Component/CompareListSection.jsx
+++ src/Component/CompareListSection.jsx
@@ -28,11 +28,12 @@ export default function CompareListSection({baseCurrency, options}){
     <div className='flex flex-col  gap-[4px] bg-neutral-600 rounded-full border-none '>
       {currentData.map((currency)=>(
       <ul key={currency.code} className='flex flex-col gap-[4px] bg-neutral-600 text-neutral-200 p-[12px] px-[8px] rounded'>
-        <li className='flex items-center justify-between gap-[4px] p-[8px] px-[12px]'>
+        <li className='flex items-center justify-center text-[28px] gap-[36px] p-[8px] px-[12px]'>
           {getFlag(currency.code) && (
           <img src={getFlag(currency.code)} className='w-[25px] h-[25px] border-none rounded' alt={currency.code}/>
         )}
-          <span className='flex gap-[12px]>{currency.base} {currency.code}</span></li>
+          <span className='flex >{currency.base}</span>
+          <span></span>{currency.code}</span></li>
       </ul>
     ))}                
   </div>
