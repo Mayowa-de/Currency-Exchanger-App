e14d752ea7f3ffc1bb41b2cea61945a50aca0d42
Mayowa-de <mayortech3@gmail.com>
2026-07-03 10:37:28 +0100

    Fix import path for currency flags

--- src/Component/CompareListSection.jsx
+++ src/Component/CompareListSection.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import {getFlag} from './crrencyFlag'
+import {getFlag} from './crrencyFlags'
 
 
 export default function CompareListSection({baseCurrency, options}){
