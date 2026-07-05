c9de4c53fed0293d1afde3c87e181c19711c9fe6
Mayowa-de <mayortech3@gmail.com>
2026-07-05 16:20:59 +0100

    Remove unnecessary class from list item

--- src/Component/CompareListSection.jsx
+++ src/Component/CompareListSection.jsx
@@ -25,7 +25,7 @@ export default function CompareListSection({baseCurrency, options}){
     <div className='flex flex-col  gap-[4px] bg-neutral-700 rounded border-none p-[8px] px-[8px] '>
       {currentData.map((currency)=>(
       <ul key={currency.code} className='flex flex-col gap-[4px] p-[12px] px-[8px] rounded'>
-        <li className='flex items-center bg-neutral-900 border-neutral-600 border-[2px] focus:border-[yellow] text-neutral-500 px-[16px] p-[12px] justify-between text-[18px] gap-[20px] rounded-[10px] '>
+        <li className='flex bg-neutral-900 border-neutral-600 border-[2px] focus:border-[yellow] text-neutral-500 px-[16px] p-[12px] justify-between text-[18px] gap-[20px] rounded-[10px] '>
           <div className='flex gap-[6px] items-center'>
           {getFlag(currency.code) && (
           <img src={getFlag(currency.code)} className='w-[15px] h-[15px] border-none rounded' alt={currency.code}/>
