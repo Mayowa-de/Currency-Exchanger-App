db5ee7d0149914efd31a87bad08ea899dd368c66
Mayowa-de <mayortech03@gmail.com>
2026-07-10 04:52:44 -0700

    adjust the header in CompareListSection font-size

--- src/Component/CompareListSection.jsx
+++ src/Component/CompareListSection.jsx
@@ -25,7 +25,7 @@ export default function CompareListSection({ baseCurrency, options }) {
     fetchData()
   }, [baseCurrency])
 
-  //currency name fetch directly frankfurter api
+  //currency name fetch directly from frankfurter api
   useEffect(() => {
     fetch('https://api.frankfurter.dev/v1/currencies')
       .then((res) => res.json())
@@ -35,13 +35,13 @@ export default function CompareListSection({ baseCurrency, options }) {
   return (
     <div className='flex flex-col  gap-[2px] bg-neutral-800 rounded-[16px] border-none p-[8px] px-[8px] justify-center items-center'>
       <div className='flex gap-[12px] px-[10px] p-[8px]  justify-between w-full'>
-        <div className='flex gap-[12px] '>
-          <h1 className='text-neutral-500'>MULTI-CURRENCY</h1>
+        <div className='flex gap-[8px] text-[16px] '>
+          <h1 className='text-neutral-500 text-[16px] '>MULTI-CURRENCY</h1>
           <h2 className='text-neutral-200'>1000</h2>
           <h2 className='text-neutral-200'>FROM</h2>
           <h3 className='text-neutral-200'>USD</h3>
         </div>
-        <h3 className='text-neutral-500'> 8 pairs</h3>
+        <h3 className='text-neutral-500 text-[12px]'> 8 pairs</h3>
       </div>
       {currentData.slice(1, 9).map((currency) => (
         <ul key={currency.code} className='flex flex-col justify-center gap-[4px] p-[2px] px-[8px] rounded w-full'>
