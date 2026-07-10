43cf1b6ce60f2f1650ce4cf3f64145803ae4c914
Mayowa-de <mayortech03@gmail.com>
2026-07-10 04:10:27 -0700

    change the ListButtons.jsx render name from ListHistoryButton to ListsButton

--- src/Component/Section.jsx
+++ src/Component/Section.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ListHistoryButton from './ListButtons'
+import ListsButton from './ListButtons'
 
 
 export default function HistorySection({ baseCurrency, baseReceiveCurrency, options }) {
 
   return (
     <div className='flex flex-col md:w-[1036px] px-[2px] w-full md:h-[540px] gap-[20px] '>
-      <ListHistoryButton baseCurrency={baseCurrency} baseReceiveCurrency={baseReceiveCurrency} options={options} />
+      <ListsButton baseCurrency={baseCurrency} baseReceiveCurrency={baseReceiveCurrency} options={options} />
     </div>
   )
 }
