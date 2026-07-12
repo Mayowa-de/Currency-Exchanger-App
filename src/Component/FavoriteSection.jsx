15deecfaf55c3296f54a6ed2d9e646c9d14f06ac
Mayowa-de <mayortech03@gmail.com>
2026-07-12 09:29:36 -0700

    added the favoriteSection logic and styling

--- src/Component/FavoriteSection.jsx
+++ src/Component/FavoriteSection.jsx
@@ -3,8 +3,11 @@ import { getFlag } from './currencyFlags'
 import usePageTitle from './hooks/usePageTitle'
 import useFavorite from './hooks/useFavorite'
 import StarIconFilled from '../assets/images/icon-star-filled.svg'
+import { ChevronDown } from 'lucide-react'
 
-export default function FavoritesSection() {
+
+export default function FavoritesSection({baseCurrency}) {
+  const [currentData, setCurrentData] = useState([])
   const [currencyName, setCurrencyName] = useState({})
   const { favoriteList, removeFavorite } = useFavorite()
 
@@ -15,6 +18,24 @@ export default function FavoritesSection() {
       .then((res) => res.json())
       .then((data) => setCurrencyName(data))
   }, [])
+
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const apiUrl = `https://currency-exchanger-app-backend.onrender.com/api?base=${encodeURIComponent(baseCurrency)}` || 'http://localhost:3000/api'
+          const response = await fetch(apiUrl)
+          const data = await response.json()
+  
+          setCurrentData(
+            data.currencies
+          )
+          console.log('currentData:', currentData)
+        } catch (error) {
+          console.log("Error can't fetch api", error)
+        }
+      }
+      fetchData()
+    }, [baseCurrency])
 
   if (favoriteList.length === 0) {
     return (
@@ -46,7 +67,7 @@ export default function FavoritesSection() {
             key={`${pair.baseCurrency}-${pair.baseReceiveCurrency}`}
             className='flex bg-neutral-900 border-neutral-600 border-[2px] h-[61px] text-neutral-500 px-[16px] p-[10px] justify-between text-[18px] rounded-[10px]'
           >
-            <div className='flex gap-[10px] justify-center items-center'>
+            <div className='flex gap-[10px] justify-center items-center border-none'>
               {getFlag(pair.baseReceiveCurrency) && (
                 <img
                   src={getFlag(pair.baseReceiveCurrency)}
@@ -54,23 +75,30 @@ export default function FavoritesSection() {
                   alt={`${pair.baseReceiveCurrency} flag`}
                 />
               )}
-              <div className='flex flex-col'>
+              <div className='flex '>
                 <span className='flex text-neutral-50'>
                   {pair.baseCurrency} → {pair.baseReceiveCurrency}
                 </span>
-                <span className='text-neutral-200'>
-                  {currencyName[pair.baseReceiveCurrency]}
-                </span>
               </div>
             </div>
-
+            <div className='flex  gap-[6px] justify-center items-center'>
+              <div className='flex flex-col items-center'>
+                <span className='text-[16px]'>
+                  {currentData?.rate}
+                </span>
+             <span className={`${currentData?.percentChange > 0 ? 'text-green-500' : 'text-red-500 flex text-[10px]'} text-[12px]`}>
+              {currentData?.percentChange?.toFixed(2)}
+            </span>
+            </div>
             <button
               onClick={() => removeFavorite(pair)}
               aria-label={`Remove ${pair.baseCurrency} to ${pair.baseReceiveCurrency} from favorites`}
-              className='bg-neutral-900 w-full p-[8px] border-lime-500 border-[1px] rounded-full'
+              className='bg-neutral-900 w-full p-[8px] border-lime-500 border-[1px] rounded'
             >
-              <img src={StarIconFilled} alt='' className='w-[12.53px] h-[12px]' />
+              <img src={StarIconFilled} alt='' className='w-[16px] h-[16px]' />
             </button>
+
+            </div>
           </li>
         ))}
       </ul>
