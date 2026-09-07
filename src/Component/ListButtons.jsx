ce3b79c26941cf520b3c764baca04b78a47ee2fa
mayowa ojomu <mayortech3@gmail.com>
2026-09-06 21:08:47 -0700

    give the container ListButton gap-0 for the desktop screen

--- src/Component/ListButtons.jsx
+++ src/Component/ListButtons.jsx
@@ -43,7 +43,7 @@ export default function ListHistoryButton({ baseCurrency, options, baseReceiveCu
   }, [])
 
   return (
-    <div className='laptop:w-[1036px] laptop:gap-[20px] mt-2 flex flex-col gap-[2px]'>
+    <div className='laptop:w-[1036px] laptop:gap-0 mt-2 flex flex-col gap-[2px]'>
       {/* Desktop tabs */}
       <ul className='md:flex hidden gap-[4px]  text-[#FFFF]'>
         {ListTitle.map((tab) => (
